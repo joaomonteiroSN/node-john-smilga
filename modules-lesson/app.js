9e3c8a931b43272826f195e41827084e6d616d18
joao monteiro <70484799+joaomonteiroSN@users.noreply.github.com>
2025-04-10 10:30:00 -0300

    mais conteudo basico do node

--- modules-lesson/app.js
+++ modules-lesson/app.js
@@ -1,5 +1,9 @@
 const names = require('./names.js')
 const sayHi = require('./3-modules.js')
 
+
+//quando a função foi invocada no arquivo de origem ele é executada aqui
+require('./mind-grenade')
+
 sayHi(names.john)
 sayHi(names.monteiro)
---
+++ modules-lesson/mind-grenade.js
@@ -0,0 +1,8 @@
+const num = 10;
+const numTwo = 10;
+
+const addValue = () => {
+    console.log(`A soma dos valores é: ${num + numTwo}`)
+}
+
+addValue();
